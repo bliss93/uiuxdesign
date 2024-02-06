176b9b61c05fb0c7d1b1ab75dfbbf58c11da3dbf
bliss93 <157003469+bliss93@users.noreply.github.com>
2024-02-06 18:16:42 +0900

    .

--- test/java/main.js
+++
@@ -1,5 +0,0 @@
-<li class="swiper-slide magazine__item magazine__item--circle swiper-slide-active" data-magazine-item="2" style="background-color: rgb(210, 194, 80); translate: none; rotate: none; scale: none; opacity: 1; visibility: inherit; transform: translate(0px, 0px);" data-swiper-slide-index="2">
-	                            <img src="https://minfo.lotteshopping.com/content/mazin/20231229/MAM00000000000000117/20231229105304961_7.jpg" alt="">
-	                        </li>
-
-.btn_wrap = "swiper-slide"
