906567fa82dd89ea5e06c84207af65ebd61b04d1
bliss93 <157003469+bliss93@users.noreply.github.com>
2024-02-15 13:02:56 +0900

    240215_01

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -25,6 +25,40 @@ $(document).ready(function(){
 	},
 
 });
-swiper.autoplay.stop();  /* 일시정지 기능 */
-swiper.autoplay.start();  /* 재생 기능 */
+
+
+/* 정지버튼을 누르면 팝업 정지-재생버튼이 보임, 정지버튼은 숨김
+	재생버튼을 누르면 팝업 재생 - 재생버튼 사라짐, 정지버튼보임*/
+
+	$('.visual .swiper .stop').on('click', function(){
+		swiper.autoplay.stop();  /* 일시정지 기능 */
+		$(".visual .swiper .play").show();
+		$(".visual .swiper .stop").hide();
+	})
+
+	$('.visual .swiper .play').on('click', function(){
+		swiper.autoplay.start();  /* 재생 기능 */
+		$(".visual .swiper .play").hide();
+		$(".visual .swiper .stop").show();
+	})
+
+
+	let scrolling //브라우저가 스크롤 된 값을 저장
+
+	function scroll_chk(){ //함수의 선언
+		scrolling = $(window).scrollTop()
+		console.log(scrolling)
+		if(scrolling > 0){ //스크롤을 내렸을 때
+			$('.header').addClass('fixed')
+		}else{ //스크롤이 맨 위에 있는 경우
+			$('.header').removeClass('fixed')
+		}
+	}
+
+	scroll_chk()//함수의 실행_브라우저가 로딩 되었을 때 단 1번 실행
+
+	$(window).scroll(function(){
+		scroll_chk() //함수의 실행_스크롤할 때마다
+	})
+
 })
