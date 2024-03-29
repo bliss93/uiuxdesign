b2b3bb7a0ff8c9349623bee2ab1f42f2e9219496
bliss93 <157003469+bliss93@users.noreply.github.com>
2024-03-30 07:49:12 +0900

    240330

---
+++ exam_fin/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.board .layer_open').on('click', function(){
+        $('.layer_popup').show()
+    })
+    $('.layer_popup .close').on('click', function(){
+        $('.layer_popup').hide()
+    })
+    
+
+
+
+
+})
