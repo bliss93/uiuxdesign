07beb3813fb6ba87b89f0b01a7196626d610ad0d
bliss93 <157003469+bliss93@users.noreply.github.com>
2024-04-01 18:09:55 +0900

    240401

--- bong2/js/main.js
+++ bong2/js/main.js
@@ -25,12 +25,26 @@ $(document).ready(function(){
 
 
     const product_swiper = new Swiper('.product .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 6, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 20, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+            1320: {
+                slidesPerView: 5,
+                spaceBetween: 20,
+            },
+            1640: {
+                slidesPerView: 6, /* 사이즈제한 */
+                spaceBetween: 20,
+            },
+        },
         
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
-        autoplay: {  /* 팝업 자동 실행 */
+        autoplay: { 
         delay: 2500,
         disableOnInteraction: true,
         },
