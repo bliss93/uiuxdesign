2b7f7b7164eb9503f8b6931578b99aa398fa2f5a
bliss93 <157003469+bliss93@users.noreply.github.com>
2024-04-03 14:01:29 +0900

    240403

--- bong2/js/main.js
+++ bong2/js/main.js
@@ -52,7 +52,7 @@ $(document).ready(function(){
     
     const social_swiper = new Swiper('.social .swiper', { /* 팝업을 감싼는 요소의 class명 */
         slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 40, /* 팝업과 팝업 사이 여백 */
+        spaceBetween: 20, /* 팝업과 팝업 사이 여백 */
         loop: true,
 
         breakpoints: {
@@ -63,7 +63,6 @@ $(document).ready(function(){
             },
             
         },
-        slidesOffsetBefore : 20,
         slideToClickedSlide : true,
 
         navigation: {  /* 이전, 다음 버튼 */
